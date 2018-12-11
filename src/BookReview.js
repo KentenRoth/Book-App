f5fd67a24bfd434780a1ea695640f06788a72b55
kentenroth <kentencroth@yahoo.com>
2018-12-10 18:21:28 -0600

    WIP: Setup React-Router and added Book Review component

---
+++ src/BookReview.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const BookReview = ({ books }) => {
+    return (
+        <div>
+            <h1>Book Title</h1>
+        </div>
+    )
+
+
+}
+
+export default BookReview
--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import BookReview from './BookReview'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route} from 'react-router-dom'
 import 'tachyons'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = (
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={App} exact={true} />
+                <Route path="/book" component={BookReview} />
+            </div>
+        </BrowserRouter>
+
+)
+
+
+ReactDOM.render(routes, document.getElementById('root'));
 
 serviceWorker.unregister();
